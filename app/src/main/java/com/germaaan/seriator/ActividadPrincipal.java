90b20ba3132b586b43136052c9b9649aeccce9d8
German Martinez <germaaan@gmail.com>
2016-01-07 16:44:09 +0100

    Añade asignación de botones de la pantalla de pregunta

--- app/src/main/java/com/germaaan/seriator/ActividadPrincipal.java
+++ app/src/main/java/com/germaaan/seriator/ActividadPrincipal.java
@@ -1,12 +1,40 @@
 package com.germaaan.seriator;
 
 import android.app.Activity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class ActividadPrincipal extends Activity {
+    private static int NUM_PREGUNTAS = 4;
+
+    private DBPref db;
+
+    private TextView pregunta;
+    private Button opcion_1;
+    private Button opcion_2;
+    private Button opcion_3;
+    private Button opcion_4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.actividad_principal);
+
+        this.pregunta = (TextView) this.findViewById(R.id.pregunta);
+        this.opcion_1 = (Button) this.findViewById(R.id.btn_opcion_1);
+        this.opcion_2 = (Button) this.findViewById(R.id.btn_opcion_2);
+        this.opcion_3 = (Button) this.findViewById(R.id.btn_opcion_3);
+        this.opcion_4 = (Button) this.findViewById(R.id.btn_opcion_4);
+
+        this.db = new DBPref(this);
+        Cursor preguntas = this.db.getPreguntas(DBPref.Categoria.HISTORIA, DBPref.Dificultad.FACIL, ActividadPrincipal.NUM_PREGUNTAS);
     }
 }
--- app/src/main/java/com/germaaan/seriator/DBPref.java
+++ app/src/main/java/com/germaaan/seriator/DBPref.java
@@ -7,13 +7,8 @@ import android.database.Cursor;
 public class DBPref extends DBHelper {
     public static enum Categoria {
         HISTORIA('A'),
-        GEOGRAFIA('B'),
-        LITERATURA('C'),
-        ARTE('D'),
-        DEPORTES('E'),
-        CIENCIA('F'),
+        //...
         TECNOLOGÍA('G'),
-        ASTRONOMÍA('H'),
         OTROS('Z');
 
         public final char C;
@@ -57,28 +52,6 @@ public class DBPref extends DBHelper {
         for (int i = 0; i < preguntas.length; i++) {
             this.addRegistro(preguntas[i]);
         }
-    }
-
-    public void updateRegistro(Pregunta[] id, Pregunta question) {
-        String filtro = "_id=" + id;
-        ContentValues valores = new ContentValues();
-        valores.put("pregunta", question.getPregunta());
-        valores.put("respuesta_correcta", question.getRespuesta());
-
-        String[] respuestas_incorrectas = question.getRespuestasIncorrectas();
-        for (int i = 0; i < respuestas_incorrectas.length; i++) {
-            valores.put("respuesta_incorrecta_" + i, respuestas_incorrectas[i]);
-        }
-
-        db.update("preguntas", valores, filtro, null);
-    }
-
-    public Cursor getPreguntas(Categoria c, Dificultad d) {
-        return this.db.rawQuery("SELECT `pregunta`, `respuesta_correcta`, `respuesta_incorrecta_1`, " +
-                        "`respuesta_incorrecta_2`, `respuesta_incorrecta_3`, `pregunta_tipo` " +
-                        "FROM `preguntas` WHERE categoria = ? AND dificultad = ? " +
-                        "ORDER BY RANDOM() LIMIT 1",
-                new String[]{String.valueOf(c.C), String.valueOf(d.D)});
     }
 
     public Cursor getPreguntas(Categoria c, Dificultad d, int limit) {
